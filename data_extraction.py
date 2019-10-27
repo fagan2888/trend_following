425d44f9af7aae04518e8c8b86e85031138c271d
Justin Goheen <j.goheen@protonmail.com>
2019-10-27 10:14:41 -0400

    updating

--- data_extraction.py
+++ data_extraction.py
@@ -27,29 +27,6 @@ def scraping_links_from_urls(url):
     return links
 
 
-def get_cme_settle(ftp_url='ftp.cmegroup.com', ftp_cwd='pub/settle', download_path='ftp_cme_downloads/'):
-    ftp = ftplib.FTP(ftp_url)
-    ftp.login()
-    ftp.cwd(ftp_cwd)
-    now = date.today()
-    yesterday = now - timedelta(1)
-    already_downloaded = pd.read_csv(f'all_files_cme_{yesterday}', index_col=0, names=['files'])
-    already_downloaded = already_downloaded['files'].to_list()
-    todays_files = ftp.nlst()
-    files_to_download = [i for i in todays_files if i not in already_downloaded]
-    for i in files_to_download:
-        wget.download(f'{ftp_url}/{ftp_cwd}/{i}', download_path)
-        if i.endswith('.zip'):
-            ZipFile(f'{i}', 'r').extractall(download_path)
-        already_downloaded = already_downloaded.append(i)
-
-    # save new already_downloaded list to file
-    pd.DataFrame(already_downloaded).to_csv(f'all_files_cme_{now.day}')
-    # delete old already_downloaded list
-
-    # close ftp
-    ftp.close()
-
 
 def get_and_unzip(url_of_file, destination_path, zipped_file_path):
     wget.download(url_of_file, destination_path)
